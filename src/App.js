9e177726463bd925be5f0f9dc8a5d9ade1c67088
FJavohr <115878920+FJavohr@users.noreply.github.com>
2023-05-20 10:41:01 +0600

    start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/Navbar.jsx";
+import { Button, DatePicker, Typography, Space, Layout } from "antd";
+import { Routes, Route, Link } from "react-router";
+const App = () => (
+  <>
+    <div className="app">
+      <div className="navbar"></div>
+      <div className="main"></div>
+      <div className="footer"></div>
+    </div>
+  </>
+);
+
+export default App;
